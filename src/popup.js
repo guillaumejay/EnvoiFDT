ab1ba24b76fba729ec9b77d537cc40ca76faed29
guillaumejay <guillaumejay@users.noreply.github.com>
2014-04-04 07:25:14 -0400

    Minor corrections

--- src/popup.js
+++ src/popup.js
@@ -1,22 +1,17 @@
 function sendEmail() {
-  var signature=document.getElementById('signature').value;
-  var toEmail=document.getElementById('toEmail').value;
-  var body=document.getElementById('emailBody').value;
-  body+="\r\n" + signature;
-  var emailUrl= "mailto:" + toEmail + "?subject=" + encodeURIComponent("Ma Feuille De Temps") + "&body=";
-  emailUrl+=encodeURIComponent(body);
-  console.log(emailUrl);
-console.log(body)
-console.log(toEmail);
- var wnd = window.open(emailUrl,"_blank");
-setTimeOut(function() {    wnd.close();}, 500);
-  
+	var signature=document.getElementById('signature').value;
+	var toEmail=document.getElementById('toEmail').value;
+	var body=document.getElementById('emailBody').value;
+	chrome.storage.sync.set({"signature": signature});
+	chrome.storage.sync.set({"toEmail": toEmail});
+	body+="\r\n" + signature;
+	var emailUrl= "mailto:" + toEmail + "?subject=" + encodeURIComponent("Ma Feuille De Temps") + "&body=";
+	emailUrl+=encodeURIComponent(body);
+	var wnd = window.open(emailUrl,"_blank");
+	setTimeOut(function() { wnd.close();}, 500);  
 }
-//https://developer.chrome.com/extensions/tut_oauth
+
 chrome.extension.onRequest.addListener(function(infoList) {
-	// var element=document.getElementById('totalh');
-	// element.innerHTML=infoList[7].number;
-	console.log('test');
 	var texte="Bonjour\r\n\r\nVoici ma feuille de temps :\r\n" + infoList[0].txtDate + "&agrave" + infoList[6].txtDate;
 	texte+="\r\n\r\n";
 	for (var i=0;i<8;i++)
@@ -24,16 +19,14 @@ chrome.extension.onRequest.addListener(function(infoList) {
 		texte+=infoList[i].txtDate + " : " + infoList[i].number + "\r\n";
 	}
 	var element=document.getElementById('emailBody');
-	texte+="\r\nBonne journ&eacute;e";
+	texte+="\r\nMerci et bonne journ&eacute;e";
 	element.innerHTML=texte;
 });
-
 
 document.addEventListener('DOMContentLoaded', function () {
   chrome.windows.getCurrent(function (currentWindow) {
     chrome.tabs.query({active: true, windowId: currentWindow.id},
                       function(activeTabs) {
-					  console.log("et la haut");
       chrome.tabs.executeScript(
         activeTabs[0].id, {file: 'contentscript.js', allFrames: false});
     });
@@ -52,9 +45,6 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 	document.getElementById('toEmail').value=result.toEmail;
   });
-
 });
-
- 
   document.getElementById('sendEmail').onclick = sendEmail;
  });
